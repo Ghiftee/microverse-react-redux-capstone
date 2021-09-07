a31dd6111cdc685af5c28503515693c2a1d12017
Ghiftee <gigibetine@gmail.com>
2021-09-07 15:17:54 +0100

    Display list of items from API

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,31 @@
+import { useSelector } from 'react-redux';
+import { countries, totalConfirmed } from '../redux/covidData/covidData';
+
 export default function Home() {
+  const countryComponents = useSelector(countries).map((country) => (
+    <div key={country.id}>
+      <h5>{country.name.toUpperCase()}</h5>
+      <p>
+        {
+          Number(country.today_confirmed).toLocaleString()
+        }
+        {' '}
+        Cases
+      </p>
+    </div>
+  ));
   return (
-    <>Home Page</>
+    <div className="pt-3 bg-pink2">
+      <div>
+        <h1>GLOBAL</h1>
+        <p>
+          {Number(useSelector(totalConfirmed)).toLocaleString()}
+          {' '}
+          Total Cases
+        </p>
+      </div>
+      <h6 className="text-white fw-bold m-0 p-2">STATS BY COUNTRY</h6>
+      {countryComponents}
+    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import store from './redux/configureStore';
+import { fetchData } from './redux/covidData/covidData';
+
+store.dispatch(fetchData);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+reportWebVitals();
