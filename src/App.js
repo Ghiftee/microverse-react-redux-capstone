6e896ac381386d1347ad6123fb3750201e7f5092
Ghiftee <gigibetine@gmail.com>
2021-09-07 10:16:56 +0100

    Prepare app routes and navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import {
+  Route, Switch,
+} from 'react-router-dom';
 import './App.css';
+import Details from './components/Details';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App" />
+    <>
+      <header>
+        <h3 className="m-0">COVID-19 STATS</h3>
+      </header>
+      <main>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Details.jsx
@@ -0,0 +1,5 @@
+export default function Details() {
+  return (
+    <>Details Page</>
+  );
+}
