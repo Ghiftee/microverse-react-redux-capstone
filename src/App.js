b3ab1a8bec04e1bae24bc46dea8e9d6606ebe364
Ghiftee <gigibetine@gmail.com>
2021-09-07 16:08:28 +0100

    Style app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import { useEffect } from 'react';
+import { Navbar } from 'react-bootstrap';
 import {
-  Route, Switch,
+  Link, Route, Switch, useLocation,
 } from 'react-router-dom';
 import './App.css';
 import Details from './components/Details';
 import Home from './components/Home';
 
 function App() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   return (
     <>
       <header>
-        <h3 className="m-0">COVID-19 STATS</h3>
+        <Navbar className="px-4 text-white bg-pink1 d-flex justify-content-between">
+          <Link to="/" className="text-decoration-none text-white fw-bold">
+            &#60; HOME
+          </Link>
+          <h3 className="m-0">COVID-19 STATS</h3>
+          <span>
+            <i className="me-4 fas fa-microphone" />
+            <i className="fas fa-cog" />
+          </span>
+        </Navbar>
       </header>
       <main>
         <Switch>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,62 @@
+import { Col, Row } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import { LinkContainer } from 'react-router-bootstrap';
 import { countries, totalConfirmed } from '../redux/covidData/covidData';
+import World from './world.svg';
 
 export default function Home() {
-  const countryComponents = useSelector(countries).map((country) => (
-    <div key={country.id}>
-      <h5>{country.name.toUpperCase()}</h5>
-      <p>
-        {
-          Number(country.today_confirmed).toLocaleString()
-        }
-        {' '}
-        Cases
-      </p>
-    </div>
+  const countryComponents = useSelector(countries).map((country, index) => (
+    <LinkContainer key={country.id} to={`/details/${country.id}`}>
+      <Col
+        xs={6}
+        sm={6}
+        md={3}
+        className={`
+          d-flex flex-column justify-content-between
+          align-items-end
+          ${([1, 0, 0, 1][index % 4]) ? 'bg-pink1' : 'bg-pink2'}
+        `}
+      >
+        <i className="far fa-arrow-alt-circle-right text-white mt-1 mb-4 h5" />
+        <span
+          className="d-flex flex-column align-items-end text-white mt-4"
+          style={{ cursor: 'pointer' }}
+        >
+          <h5 className="d-inline-block m-0 text-end fw-bold">{country.name.toUpperCase()}</h5>
+          <p>
+            {
+              Number(country.today_confirmed).toLocaleString()
+            }
+            {' '}
+            Cases
+          </p>
+        </span>
+      </Col>
+    </LinkContainer>
   ));
   return (
     <div className="pt-3 bg-pink2">
-      <div>
-        <h1>GLOBAL</h1>
-        <p>
-          {Number(useSelector(totalConfirmed)).toLocaleString()}
-          {' '}
-          Total Cases
-        </p>
-      </div>
-      <h6 className="text-white fw-bold m-0 p-2">STATS BY COUNTRY</h6>
-      {countryComponents}
+      <Row className="m-0">
+        <Col xs={6} sm={6} md={6} className="d-flex justify-content-end">
+          <img
+            src={World}
+            alt="World"
+            height="150px"
+          />
+        </Col>
+        <Col xs={6} sm={6} md={6} className="p-0 text-white d-flex flex-column justify-content-center">
+          <h1 className="fw-bold m-0">GLOBAL</h1>
+          <p>
+            {Number(useSelector(totalConfirmed)).toLocaleString()}
+            {' '}
+            Total Cases
+          </p>
+        </Col>
+      </Row>
+      <Row className="m-0 mt-4 bg-pink1" style={{ cursor: 'pointer' }}>
+        <h6 className="text-white fw-bold m-0 p-2">STATS BY COUNTRY</h6>
+        {countryComponents}
+      </Row>
     </div>
   );
 }
